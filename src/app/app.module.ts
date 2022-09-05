5a51f2e9f7967e483780bc301db5a6b73f8fbffd
TCHAKAL <ahmed.tchakal@gmail.com>
2022-09-05 10:31:03 +0100

    Ajouter la page d'inscription IHM

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PageLoginComponent } from './pages/page-login/page-login.component';
+import { PageInscriptionComponent } from './pages/page-inscription/page-inscription.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PageLoginComponent
+    PageLoginComponent,
+    PageInscriptionComponent
   ],
   imports: [
     BrowserModule,
