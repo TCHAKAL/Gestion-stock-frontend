1424bb7aec56032db971eb12e1152166febdff6d
TCHAKAL <ahmed.tchakal@gmail.com>
2022-09-05 11:00:14 +0100

    Ajouter le routage entre dashborad et son fils statistiques

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,17 +3,18 @@ import {RouterModule, Routes} from '@angular/router';
 import {PageLoginComponent} from "./pages/page-login/page-login.component";
 import {PageInscriptionComponent} from "./pages/page-inscription/page-inscription.component";
 import {PageDashboardComponent} from "./pages/page-dashboard/page-dashboard.component";
+import {PageStatistiquesComponent} from "./pages/page-statistiques/page-statistiques.component";
 
 const routes: Routes = [
   {path: 'login', component: PageLoginComponent},
   {path: 'inscrire', component: PageInscriptionComponent},
-  // {
-  //   path: '',
-  //   component: PageDashboardComponent,
-  //   children: [
-  //     {path: 'articles'}
-  //   ]
-  // }
+  {
+    path: '',
+    component: PageDashboardComponent,
+    children: [
+      {path: 'statistiques', component: PageStatistiquesComponent}
+    ]
+  }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { PageLoginComponent } from './pages/page-login/page-login.component';
 import { PageInscriptionComponent } from './pages/page-inscription/page-inscription.component';
 import { PageDashboardComponent } from './pages/page-dashboard/page-dashboard.component';
+import { PageStatistiquesComponent } from './pages/page-statistiques/page-statistiques.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PageLoginComponent,
     PageInscriptionComponent,
-    PageDashboardComponent
+    PageDashboardComponent,
+    PageStatistiquesComponent
   ],
   imports: [
     BrowserModule,
