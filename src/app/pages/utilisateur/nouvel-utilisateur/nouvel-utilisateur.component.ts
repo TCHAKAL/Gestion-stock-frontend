6a1e1126a52a30557bf37f7607dfd5e6638f7b51
TCHAKAL <ahmed.tchakal@gmail.com>
2022-09-07 15:28:19 +0100

    Développement de l'interface nouvel-utilisateur IHM

---
+++ src/app/pages/utilisateur/nouvel-utilisateur/nouvel-utilisateur.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nouvel-utilisateur',
+  templateUrl: './nouvel-utilisateur.component.html',
+  styleUrls: ['./nouvel-utilisateur.component.scss']
+})
+export class NouvelUtilisateurComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
